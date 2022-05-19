50cb2c0b43b4adcf39b37c8562a09b7b3bfb7951
moon990519 <moon990519@gmail.com>
2022-05-19 14:15:08 -0300

    direct link added

--- default_page/views.py
+++ default_page/views.py
@@ -4,3 +4,27 @@ from django.http import HttpResponse
 
 def home_views(request):
     return render(request, 'index.html')
+
+def category_books_page_views(request):
+    return render(request, 'category_books_page/index.html')
+
+def category_clothings_page_views(request):
+    return render(request, 'category_clothings_page/index.html')
+
+def category_electronics_page_views(request):
+    return render(request, 'category_electronics_page/index.html')
+
+def category_housings_page_views(request):
+    return render(request, 'category_housings_page/index.html')
+
+def category_musical_instruments_page_views(request):
+    return render(request, 'category_musical_instruments_page/index.html')
+
+def category_sports_page_views(request):
+    return render(request, 'category_sports_page/index.html')
+
+def category_stationaries_page_views(request):
+    return render(request, 'category_stationaries_page/index.html')
+
+def category_tickets_page_views(request):
+    return render(request, 'category_tickets_page/index.html')
