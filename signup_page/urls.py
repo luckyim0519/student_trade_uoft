547ae0c86e62f815acad79a29448097239cc0c06
moon990519 <moon990519@gmail.com>
2022-05-28 20:22:58 -0300

    sign up page to be editted

--- signup_page/urls.py
+++ signup_page/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_views),
+    path('login_page', views.login_page_views, name='login_page'),
 ]
--- signup_page/views.py
+++ signup_page/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_views(request):
     return render(request, 'signup_page/index.html')
+
+def login_page_views(request):
+    return render(request, 'login_page/index.html')
