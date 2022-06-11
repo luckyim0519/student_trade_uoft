ecb3d5e6b21b601c3b5219592b344a9e8dc407f5
moon990519 <moon990519@gmail.com>
2022-06-11 05:43:28 -0300

    models.py has bee nchangeed

--- signup_page/models.py
+++ signup_page/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class signupForm(models.Model):
     username = models.CharField(max_length=30, null=False, blank=False)
-    email = models.EmailField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False, primary_key=True)
     password=models.CharField(max_length=25, null=False, blank=False)
--- signup_page/views.py
+++ signup_page/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .models import signupForm
@@ -50,22 +51,22 @@ def signup_detail(request, pk):
         obj.delete()
         return HttpResponse(status=204)
 
-#@csrf_exempt
-#def login(request):
-#    if request.method =="POST"
-#        print("request log: "+ str(request.body))
-#        id = request.POST.get('userid', '')
- #       pwd = request.POST.get('userpwd', '')
- #       print("id = "+ id + "pwd =" + pwd)
+@csrf_exempt
+def login(request):
+    if request.method =="POST":
+        print("request log: "+ str(request.body))
+        id = request.POST.get('userid', '')
+        pwd = request.POST.get('userpwd', '')
+        print("id = "+ id + "pwd =" + pwd)
 
-#        result = authenticate(username=id, password=pwd)
+        result = authenticate(username=id, password=pwd)
 
-#        if result:
-#            print("login successful!")
-#            return HttpResponse(status=200)
-#        else:
-#            print("login failed")
- #           return HttpResponse(status=401)
+        if result:
+            print("login successful!")
+            return HttpResponse(status=200)
+        else:
+            print("login failed")
+            return HttpResponse(status=401)
 
-#    return render(request, 'signup_page/index.html')
+    return render(request, 'signup_page/index.html')
 
