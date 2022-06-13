6199fe69f79cf5320078e46f34e66428f66ed2b5
moon990519 <moon990519@gmail.com>
2022-06-12 23:06:53 -0300

    db not ignored

--- category_books_page/models.py
+++ category_books_page/models.py
@@ -7,3 +7,6 @@ class BookBoardModel(models.Model):
     #to be updated after the sign up page
     pub_date= models.DateTimeField('date published')
     writer = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.title
