cb7592b86ba0bd7af71b5d9888c4486b61bd854c
googoles <ysjang0725@gmail.com>
2022-06-15 00:04:53 +0900

    Set html inheritance

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +124,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[ os.path.join(BASE_DIR, '/static/') ]
+STATICFILES_DIRS=[ os.path.join(BASE_DIR,'config','static') ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
