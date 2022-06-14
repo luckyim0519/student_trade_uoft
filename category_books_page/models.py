ad9a9cf6ddf1b81398250ee81cfb8a256f769005
moon990519 <moon990519@gmail.com>
2022-06-14 16:17:29 -0300

    connection with db success

--- category_books_page/models.py
+++ category_books_page/models.py
@@ -5,13 +5,13 @@ class BookBoardModel(models.Model):
     title = models.CharField(max_length=30, null=False)
     content = models.TextField()
     #to be updated after the sign up page
-<<<<<<< HEAD
+
     pub_date= models.DateTimeField('date published')
     writer = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.title
-=======
+
     pub_date= models.DateTimeField('date published', null=True)
     writer = models.CharField(max_length=20, null=True)
->>>>>>> 5260337 (DB tested)
+
