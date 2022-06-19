c132a448f8b8cb38fea60a679b3097642faf4425
moon990519 <moon990519@gmail.com>
2022-06-18 23:02:17 -0300

    signup index.html is changed error post

--- signup_page/views.py
+++ signup_page/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth import authenticate
+from django.contrib import auth
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import signupModel
 from .serializers import signupFormSerializer
-from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework.parsers import JSONParser
 import json
 
@@ -14,3 +16,15 @@ def signup_pageHome(request):
 def login_page(request):
     return render(request, 'login_page/index.html')
 
+@csrf_exempt
+def signupAction(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['password_check']:
+            user = signupModel.create_user(username=request.POST['username'],
+                                           email=request.POST['email'],
+                                           password=request.POST['password'],
+                                           )
+            auth.login(request, user)
+            return redirect('/')
+        return render(request, 'signup_page/index.html')
+    return render(request, 'signup_page/index.html')
