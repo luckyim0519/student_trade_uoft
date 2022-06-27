01f235ade88e2077fc8b388c46017e5cbd28a1b2
Yunsoo <googoles1@gmail.com>
2022-06-27 20:02:54 +0900

    Improve layout in postform

--- category_books_page/forms.py
+++ category_books_page/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import BookBoardModel
 
+
 class BookBoardForm(forms.ModelForm):
     class Meta:
         model = BookBoardModel
         fields =['title', 'content', 'writer']
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'writer': forms.TextInput(attrs={'class': 'form-control'})
+        }
