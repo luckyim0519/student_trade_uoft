571af789a04d68b8b8826da8bfbe298d336701a0
moon990519 <moon990519@gmail.com>
2022-06-27 08:36:45 -0300

    Revert "Improve layout in postform"
    This reverts commit 01f235ade88e2077fc8b388c46017e5cbd28a1b2.

--- category_books_page/forms.py
+++ category_books_page/forms.py
@@ -1,13 +1,7 @@
 from django import forms
 from .models import BookBoardModel
 
-
 class BookBoardForm(forms.ModelForm):
     class Meta:
         model = BookBoardModel
         fields =['title', 'content', 'writer']
-        widgets = {
-            'title': forms.TextInput(attrs={'class':'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control'}),
-            'writer': forms.TextInput(attrs={'class': 'form-control'})
-        }
